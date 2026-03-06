c3949a341ec9c0e9fc14a4ce16052023641eb0ca
John Asley Wong <johnasleyw@gmail.com>
2026-03-07 04:24:31 +0800

    Added Privacy Policy to Footer

--- export_script.py
+++ export_script.py
@@ -126,6 +126,7 @@ FILES_TO_READ = [
     'goldenbay-mobile/App.js',
     'goldenbay-mobile/index.js',
     'frontend/src/pages/public/about/PrivacyPolicyPage.jsx',
+    'goldenbay-mobile/.easignore',
 
 ]
 
