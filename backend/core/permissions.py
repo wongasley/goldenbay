b44045006c530317301b59445c64853fd90cd77b
John Asley Wong <johnasleyw@gmail.com>
2026-08-21 03:32:06 +0800

    Inventory

--- backend/core/permissions.py
+++ backend/core/permissions.py
@@ -15,14 +15,10 @@ class IsSupervisorOrAdmin(BasePermission):
         )
 
 class IsAdminUserOnly(BasePermission):
-    """
-    Allows access only to users in the 'Admin' group, or superusers.
-    """
     def has_permission(self, request, view):
-        if not request.user or not request.user.is_authenticated:
-            return False
-        
-        return (
-            request.user.is_superuser or 
-            request.user.groups.filter(name='Admin').exists()
+        return bool(
+            request.user and request.user.is_authenticated and (
+                request.user.is_superuser or 
+                request.user.groups.filter(name__in=['Admin', 'Supervisor']).exists()
+            )
         )
